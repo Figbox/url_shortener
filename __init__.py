f0e8e2cc2211cd497f0e5438c63c7635cf02cc31
劉達 <ryu@anchorz.co.jp>
2021-09-26 12:11:01 +0900

    修正

--- __init__.py
+++ __init__.py
@@ -1,24 +1,14 @@
-import random
-
 from fastapi import APIRouter, Body, Depends
-from sqlalchemy.orm import Session
 from starlette.responses import RedirectResponse
 
 from app.core.database_engine.DbAdaptor import DbAdaptor
 from app.core.module_class import TableModule, ApiModule
-from app.core.page_engine.PageAdaptor import PageAdaptor
-from app.core.database_engine.db_core import get_db
-from app.modules.sample.table import SampleTable
 from app.modules.url_shortener import urls_crud
 from app.modules.url_shortener.table import UrlShortenerTable
 
 
 class UrlShortener(ApiModule, TableModule):
     def _register_api_bp(self, bp: APIRouter):
-        @bp.post('/show_body')
-        def show_body(body: str = Body(..., embed=True)):
-            return f'your body is: {body}'
-
         # テーブル関連
         @bp.post('/create', description='create data to table')
         def create(dba: DbAdaptor = Depends(DbAdaptor(UrlShortenerTable).dba),
@@ -26,17 +16,19 @@ class UrlShortener(ApiModule, TableModule):
             """create a new url shortening"""
             return urls_crud.random_create(dba, target_url)
 
-        @bp.delete('/delete', description='delete a data')
-        def delete(id: int, dba: DbAdaptor = Depends(DbAdaptor(UrlShortenerTable).dba)):
-            return dba.delete(id)
+        # @bp.delete('/delete', description='delete a data')
+        # def delete(id: int, dba: DbAdaptor = Depends(DbAdaptor(UrlShortenerTable).dba)):
+        #     ...
 
-        # 任意なプレフィックスを作成する為
+        # 一番短いのプレフィックスのは何もないこと↓
         main_bp = self._register_free_prefix('', 'main')
 
-        @main_bp.get('/{link}', description='you used a free prefix')
+        @main_bp.get('/{link}', description='リダイレクト機能を入れている')
         def abc_sample(link: str,
                        dba: DbAdaptor = Depends(DbAdaptor(UrlShortenerTable).dba)):
-            return RedirectResponse(urls_crud.get_target_url(dba, link))
+            target_url = urls_crud.get_target_url(dba, link)
+            # ターゲットURLにリダイレクトする
+            return RedirectResponse(target_url, status_code=307)
 
     def get_table(self) -> list:
         return [UrlShortenerTable]
